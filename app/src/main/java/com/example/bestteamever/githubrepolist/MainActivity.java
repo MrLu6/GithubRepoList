1f256fb5f264df9f698d1664618f5ed5dac3008e
CHENGJUN LU <chengjunlu@CHENGJUNs-MacBook-Pro.local>
2019-04-15 23:13:29 -0700

    basic set up for search bar and listview

--- app/src/main/java/com/example/bestteamever/githubrepolist/MainActivity.java
+++ app/src/main/java/com/example/bestteamever/githubrepolist/MainActivity.java
@@ -2,12 +2,62 @@ package com.example.bestteamever.githubrepolist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView search_repo;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        search_repo = (ListView) findViewById(R.id.search_repo);
+
+        ArrayList<String> repoList = new ArrayList<>();
+        repoList.addAll(Arrays.asList(getResources().getStringArray(R.array.repo_list)));
+
+        adapter = new ArrayAdapter<String>(
+                MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                repoList
+        );
+
+        search_repo.setAdapter(adapter);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu,menu);
+        MenuItem item = menu.findItem(R.id.search_repo);
+        SearchView searchView = (SearchView)item.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                adapter.getFilter().filter(s);
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+
     }
 }
+
