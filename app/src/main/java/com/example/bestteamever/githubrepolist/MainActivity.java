916f0f10605b2c1ee47dc8ec1ef4505c9840b48d
CHENGJUN LU <chengjunlu@CHENGJUNs-MacBook-Pro.local>
2019-04-15 21:55:55 -0700

    init commit

---
+++ app/src/main/java/com/example/bestteamever/githubrepolist/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.bestteamever.githubrepolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
